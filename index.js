535dd960990fb084949aa12c4380d9587de65432
Even Sosa <esosa@akidolabs.com>
2019-12-13 07:53:35 -0800

    add is single app mode enabled

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const { Asam } = NativeModules;
 
 export const enterSingleAppMode = Asam.enterSingleAppMode; 
 export const exitSingleAppMode = Asam.exitSingleAppMode;
+export const isSingleAppModeEnabled = Asam.isSingleAppModeEnabled;
 
 export default Asam;
